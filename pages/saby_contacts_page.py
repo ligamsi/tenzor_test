14b7cbc02d8d8be1dbec9823f4c6d432629db7a6
ligamsi <galimov.ism@mail.ru>
2025-12-17 18:32:05 +0500

    fixes 4

--- pages/saby_contacts_page.py
+++ pages/saby_contacts_page.py
@@ -2,39 +2,43 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
+from time import sleep
 
 class SabyContactsPage:
-    CONTACTS_MENU = (By.XPATH, "//a[contains(text(), 'Контакты')]")
+    # Элементы меню
+    CONTACTS_MENU = (By.XPATH, "//a[.//span[text()='Контакты']]")
     TENSOR_BANNER = (By.XPATH, "//img[contains(@alt,'Tensor')]")
-    MORE_OFFICES_BUTTON = (By.XPATH, "//span[contains(text(), 'Ещё')]")
+    MORE_OFFICES_BUTTON = (By.XPATH, "//span[contains(text(),'Ещё') and contains(text(),'офис')]")
 
     def __init__(self, driver):
         self.driver = driver
         self.wait = WebDriverWait(driver, 20)
 
     def open(self):
-        self.driver.get("https://saby.ru/contacts")
+        # Открываем главную страницу Saby
+        self.driver.get("https://saby.ru/")
 
     def hover_contacts_menu(self):
-        menu = self.wait.until(EC.presence_of_element_located(self.CONTACTS_MENU))
-        ActionChains(self.driver).move_to_element(menu).perform()
+        # Наводим курсор на пункт меню 'Контакты
+        contacts = self.wait.until(EC.presence_of_element_located(self.CONTACTS_MENU))
+        ActionChains(self.driver).move_to_element(contacts).pause(0.5).perform()
 
     def click_more_offices(self):
         # Наведение на меню
         self.hover_contacts_menu()
         # Ждём кнопку и кликаем
-        button = self.wait.until(EC.presence_of_element_located(self.MORE_OFFICES_BUTTON))
-        self.driver.execute_script("arguments[0].scrollIntoView(true);", button)
-        self.wait.until(EC.element_to_be_clickable(self.MORE_OFFICES_BUTTON))
+        button = self.wait.until(EC.element_to_be_clickable(self.MORE_OFFICES_BUTTON))
         button.click()
 
     def click_tensor_banner(self):
+        # Кликаем по баннеру Tensor
         banner = self.wait.until(EC.presence_of_element_located(self.TENSOR_BANNER))
-        self.driver.execute_script("arguments[0].scrollIntoView(true);", banner)
         self.wait.until(EC.element_to_be_clickable(self.TENSOR_BANNER))
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", banner)
         banner.click()
 
     def switch_to_tensor(self):
+        # Переключаемся на новое окно Tensor
         self.wait.until(lambda d: len(d.window_handles) > 1 or "tensor.ru" in d.current_url)
         if len(self.driver.window_handles) > 1:
             self.driver.switch_to.window(self.driver.window_handles[-1])
