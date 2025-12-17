50cf3667cec7a79237bd1d4e32af7ff8aa544996
ligamsi <galimov.ism@mail.ru>
2025-12-17 13:52:34 +0500

    test_saby_tensor

---
+++ tests/test_saby_tensor.py
@@ -0,0 +1,19 @@
+from pages.saby_contacts_page import SabyContactsPage
+from pages.tensor_main_page import TensorMainPage
+
+def test_saby_to_tensor(driver):
+    saby_page = SabyContactsPage(driver)
+    saby_page.open()
+
+    saby_page.click_more_offices()
+    saby_page.click_tensor_banner()
+    saby_page.switch_to_new_tab()
+
+    assert "tensor.ru" in driver.current_url
+
+    tensor_page = TensorMainPage(driver)
+    assert tensor_page.is_power_block_present()
+
+    tensor_page.click_more_details()
+
+    assert driver.current_url == "https://tensor.ru/about"
